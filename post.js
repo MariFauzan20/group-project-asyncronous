fd72f1946e0b43d597628dc18a6391a53a71aea5
Mar'i Fauzan <marifauzan20@gmail.com>
2021-10-08 16:03:12 +0700

    add post.js

--- post.js
+++ post.js
@@ -1,25 +1,27 @@
-const params; // tempat menampung parameter yang ada
+import { getAuthor, getRandomPic } from "./helpers.js";
 
-const elPageTitle = document.querySelector('#page-title');
-const elDetailBerita = document.querySelector('#detail-berita');
-const elLoading = document.querySelector('#loading');
-const elNotFound = document.querySelector('#not-found');
-const elCardImg = document.querySelector('.card-img-top');
-const elCardText = document.querySelector('.card-text');
-const elCardAuthorImg = document.querySelector('#author-img');
-const elCardAuthorName = document.querySelector('#author-name');
-const elCardAuthorEmail = document.querySelector('#author-email');
-const elListGroup = document.querySelector('#list-group');
+const params = 1;
+
+const elPageTitle = document.querySelector("#page-title");
+const elDetailBerita = document.querySelector("#detail-berita");
+const elLoading = document.querySelector("#loading");
+const elNotFound = document.querySelector("#not-found");
+const elCardImg = document.querySelector(".card-img-top");
+const elCardText = document.querySelector(".card-text");
+const elCardAuthorImg = document.querySelector("#author-img");
+const elCardAuthorName = document.querySelector("#author-name");
+const elCardAuthorEmail = document.querySelector("#author-email");
+const elListGroup = document.querySelector("#list-group");
 
 const createListElement = (comment) => {
-  const elListItem = document.createElement('div');
-  const elListItemContainer = document.createElement('div');
-  const elListItemTitle = document.createElement('div');
-  const elListItemText = document.createElement('span');
+  const elListItem = document.createElement("div");
+  const elListItemContainer = document.createElement("div");
+  const elListItemTitle = document.createElement("div");
+  const elListItemText = document.createElement("span");
 
-  elListItem.classList.add('list-group-item');
-  elListItemContainer.classList.add('ms-2', 'me-auto');
-  elListItemTitle.classList.add('fw-bold');
+  elListItem.classList.add("list-group-item");
+  elListItemContainer.classList.add("ms-2", "me-auto");
+  elListItemTitle.classList.add("fw-bold");
 
   elListItemTitle.innerHTML = comment.email;
   elListItemText.innerHTML = comment.body;
@@ -33,6 +35,20 @@ const createListElement = (comment) => {
 
 const renderPost = async () => {
   // EDIT HERE
+  let author = await getAuthor(params);
+  let image = await getRandomPic();
+
+  console.log(author);
+  console.log(image);
+
+  if (author) {
+    elLoading.classList.add("d-none");
+    elDetailBerita.classList.remove("d-none");
+
+    elCardAuthorImg.src = image;
+    elCardAuthorName.innerText = author.name;
+    elCardAuthorEmail.innerText = author.email;
+  }
 };
 
 renderPost();
