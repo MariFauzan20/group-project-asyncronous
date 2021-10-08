8e313fba35ed64ea487f73fd57ede5fc2d43c0e3
Mar'i Fauzan <marifauzan20@gmail.com>
2021-10-08 15:59:37 +0700

    update helpers.js

--- helpers.js
+++ helpers.js
@@ -1,6 +1,6 @@
 export const getPosts = async () => {
   // EDIT HERE
-  const posts = fetch("https://jsonplaceholder.typicode.com/posts", {
+  const posts = await fetch("https://jsonplaceholder.typicode.com/posts", {
     method: "GET",
     headers: { "Content-type": "application/json;charset=UTF-8" },
   })
@@ -12,10 +12,13 @@ export const getPosts = async () => {
 
 export const getPost = async (post_id) => {
   // EDIT HERE
-  const post = fetch(`https://jsonplaceholder.typicode.com/posts/${post_id}`, {
-    method: "GET",
-    headers: { "Content-type": "application/json;charset=UTF-8" },
-  })
+  const post = await fetch(
+    `https://jsonplaceholder.typicode.com/posts/${post_id}`,
+    {
+      method: "GET",
+      headers: { "Content-type": "application/json;charset=UTF-8" },
+    }
+  )
     .then((response) => response.json())
     .catch((err) => console.log(err));
 
@@ -24,7 +27,7 @@ export const getPost = async (post_id) => {
 
 export const getPostComments = async (post_id) => {
   // EDIT HERE
-  const postComment = fetch(
+  const postComment = await fetch(
     `https://jsonplaceholder.typicode.com/posts/${post_id}/comments`,
     {
       method: "GET",
@@ -39,10 +42,32 @@ export const getPostComments = async (post_id) => {
 
 export const getAuthor = async (user_id) => {
   // EDIT HERE
+  const author = await fetch(
+    `https://jsonplaceholder.typicode.com/users/${user_id}`,
+    {
+      method: "GET",
+      headers: { "Content-type": "application/json;charset=UTF-8" },
+    }
+  )
+    .then((response) => response.json())
+    .catch((err) => console.log(err));
+
+  return author;
 };
 
 export const getPostsByAuthor = async (author_id) => {
   // EDIT HERE
+  const postByAuthor = await fetch(
+    `https://jsonplaceholder.typicode.com/posts?userId=${author_id}`,
+    {
+      method: "GET",
+      headers: { "Content-type": "application/json;charset=UTF-8" },
+    }
+  )
+    .then((response) => response.json())
+    .catch((err) => console.log(err));
+
+  return postByAuthor;
 };
 
 export const getRandomPic = async () => {
