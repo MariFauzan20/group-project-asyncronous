f115e0ed7a365f9e822ceb1a013c142f05565d21
Mar'i Fauzan <marifauzan20@gmail.com>
2021-10-08 14:12:58 +0700

    add endpoint Post & Post Comment

--- helpers.js
+++ helpers.js
@@ -1,13 +1,40 @@
 export const getPosts = async () => {
   // EDIT HERE
+  const posts = fetch("https://jsonplaceholder.typicode.com/posts", {
+    method: "GET",
+    headers: { "Content-type": "application/json;charset=UTF-8" },
+  })
+    .then((response) => response.json())
+    .catch((err) => console.log(err));
+
+  return posts;
 };
 
 export const getPost = async (post_id) => {
   // EDIT HERE
+  const post = fetch(`https://jsonplaceholder.typicode.com/posts/${post_id}`, {
+    method: "GET",
+    headers: { "Content-type": "application/json;charset=UTF-8" },
+  })
+    .then((response) => response.json())
+    .catch((err) => console.log(err));
+
+  return post;
 };
 
 export const getPostComments = async (post_id) => {
   // EDIT HERE
+  const postComment = fetch(
+    `https://jsonplaceholder.typicode.com/posts/${post_id}/comments`,
+    {
+      method: "GET",
+      headers: { "Content-type": "application/json;charset=UTF-8" },
+    }
+  )
+    .then((response) => response.json())
+    .catch((err) => console.log(err));
+
+  return postComment;
 };
 
 export const getAuthor = async (user_id) => {
@@ -20,20 +47,20 @@ export const getPostsByAuthor = async (author_id) => {
 
 export const getRandomPic = async () => {
   try {
-    const image = await fetch('https://source.unsplash.com/random/720x480');
+    const image = await fetch("https://source.unsplash.com/random/720x480");
     return image.url;
   } catch (error) {
-    console.log('getRandomPic', error);
+    console.log("getRandomPic", error);
     throw error;
   }
 };
 
 export const getRandomProfile = async () => {
   try {
-    const image = await fetch('https://source.unsplash.com/480x480/?profile');
+    const image = await fetch("https://source.unsplash.com/480x480/?profile");
     return image.url;
   } catch (error) {
-    console.log('getRandomProfile', error);
+    console.log("getRandomProfile", error);
     throw error;
   }
 };
